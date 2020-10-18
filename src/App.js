248dfca654da38f36225c3d269ada630e201dd42
Suryo Prasetyo W <the.oyrus@gmail.com>
2020-10-18 08:22:14 +0700

    1st commit quiz 3, boilerplate axios react router

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Routes from './Tugas-15/Route'
+
+function App() {
+  return (
+    <div className="App">
+      <Routes />
+    </div >
+  );
+}
+
+export default App;
